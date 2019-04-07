524eac8d481e5b8f868894722f1551c93713b90b
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-04-07 16:44:37 -0400

    Initial commit

---
+++ B9AirBrake/B9AirBrake.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace B9AirBrake
+{
+    public class B9AirBrake : ModuleAeroSurface
+    {
+        new void Start()
+        {
+            base.Start();
+
+            base.ignorePitch = true;
+            base.ignoreYaw = true;
+
+            Fields["ignorePitch"].guiActive = false;
+            Fields["ignorePitch"].guiActiveEditor = false;
+
+            Fields["ignoreYaw"].guiActive = false;
+            Fields["ignoreYaw"].guiActiveEditor = false;
+
+            Fields["aeroAuthorityLimiter"].guiActive = false;
+            Fields["aeroAuthorityLimiter"].guiActiveEditor = false;
+        }
+
+        new void LateUpdate()
+        {
+            base.LateUpdate();
+
+            // These two fields are not visible in the Start(), so need to be set here
+            Fields["deploy"].guiActive = false;
+            Fields["deploy"].guiActiveEditor = false;
+        }
+    }
+}
